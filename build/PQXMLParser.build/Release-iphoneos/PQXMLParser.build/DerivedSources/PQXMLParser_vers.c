55c9b02e50b3b52856a53bb4e613576b9c92d2b6
Alex Moskvin <amoskvin@plexteq.com>
2017-12-26 16:01:04 +0200

    Updated readme.md

--- build/PQXMLParser.build/Release-iphoneos/PQXMLParser.build/DerivedSources/PQXMLParser_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char PQXMLParserVersionString[];
- extern const double PQXMLParserVersionNumber;
-
- const unsigned char PQXMLParserVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:PQXMLParser  PROJECT:PQXMLParser-1" "\n";
- const double PQXMLParserVersionNumber __attribute__ ((used)) = (double)1.;
--- build/PQXMLParser.build/Release-iphoneos/PQXMLParser.build/Objects-normal/arm64/PQXMLParser_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Volumes/work/projects/PQXMLParser/build/PQXMLParser.build/Release-iphoneos/PQXMLParser.build/DerivedSources/PQXMLParser_vers.c
